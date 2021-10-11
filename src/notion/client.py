b5bc68c76b099e1679786b2dcfe8485056b87908
MeillieA <meilliez.antoine@gmail.com>
2021-10-11 10:54:28 +0200

    Improve SSL error management

--- src/notion/client.py
+++ src/notion/client.py
@@ -3,6 +3,8 @@ import functools
 import logging
 from omegaconf import DictConfig
 from notion_client import Client, APIResponseError, APIErrorCode
+import httpx
+import re
 
 from .config import NotionConfig
 
@@ -14,31 +16,60 @@ def notion_request(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         try:
-            return func(*args, **kwargs)
+            res = func(*args, **kwargs)
+            return res
         except APIResponseError as error:
             if error.code == APIErrorCode.ObjectNotFound:
                 logging.error(f"An error occured while calling {func.__name__} with arguments {args} {kwargs}")
                 raise APIResponseError
             else:
                 logging.exception(error)
+        except httpx.ConnectError as connection_error:
+            logger.exception(connection_error)
     return wrapper
 
 
-class NotionClient(Client):
+class NotionClient:
 
     def __init__(self, auth, config: DictConfig=None):
         if config is None:
             self._config = NotionConfig()
         else:
             self._config = NotionConfig(ROOT_URL=config.ROOT_URL, LOG_LEVEL=config.LOG_LEVEL)
-        super(NotionClient, self).__init__(auth=auth,
-                                           log_level=self._config.LOG_LEVEL)
-        logger.info("Notion client initilized")
+        try:
+            self._client = Client(auth=auth, log_level=self._config.LOG_LEVEL)
+            self._test_connection()
+        except httpx.ConnectError as e:
+            logger.warning(repr(e))
+            print(str(e))
+            if re.match(r".*SSL.*", str(e)) is not None:
+                logger.warning("Setting connection without SSL verification")
+                try:
+                    logger.info("Initializing a new Notion Client with no ssl verifications")
+                    self._client = Client(auth=auth,
+                                          log_level=self._config.LOG_LEVEL,
+                                          client=httpx.Client(verify=False)
+                                          )
+                    self._test_connection()
+                except httpx.ConnectError as e_bis:
+                    raise e_bis
+        self._test_connection()
+        logger.info("Notion client initialized")
 
+    def _test_connection(self) -> None:
+        assert self._client.users.me()
+
+    @notion_request
+    def retrieve_bot_user(self) -> dict:
+        return self._client.users.me()
+
+    @notion_request
+    def retrieve_users_list(self) -> list:
+        return self._client.users.list()["results"]
 
     @notion_request
     def retrieve_db(self, database_id: str):
-        return self.databases.retieve(database_id=database_id)
+        return self._client.databases.retrieve(database_id=database_id)
 
     @notion_request
     def query_db(self, database_id: str,
@@ -73,8 +104,13 @@ class NotionClient(Client):
 
     @notion_request
     def add_page(self, database_id: str, properties: dict, children: dict):
-        return self.pages.create(
+        return self._client.pages.create(
             parent={"database_id": database_id},
             properties=properties,
             children=children
         )
+
+    @notion_request
+    def delete_page(self):
+        #TODO
+        pass
